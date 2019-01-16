0aab4b9de5e95b85b5e28f9ea15cf74b091642e7
Roman-Shine <Roman.shaine@yandex.ru>
2019-01-16 22:03:54 +0300

    first commit

---
+++ Sass-Pug-Template/gulpfile.js
@@ -0,0 +1,110 @@
+var gulp          = require('gulp'),
+		gutil         = require('gulp-util' ),
+		sass          = require('gulp-sass'),
+		babel 				= require('gulp-babel'),
+		pug           = require('gulp-pug'),
+		browsersync   = require('browser-sync'),
+		concat        = require('gulp-concat'),
+		uglify        = require('gulp-uglify'),
+		cleancss      = require('gulp-clean-css'),
+		rename        = require('gulp-rename'),
+		autoprefixer  = require('gulp-autoprefixer'),
+		notify        = require("gulp-notify"),
+		rsync         = require('gulp-rsync');
+
+gulp.task('browser-sync', function() {
+	browsersync({
+		server: {
+			baseDir: 'app'
+		},
+		notify: false,
+		// open: false,
+		// tunnel: true,
+		// tunnel: "projectname", // Go to http://projectname.localtunnel.me
+	})
+});
+
+//Pug
+gulp.task('pug', function () {
+	return gulp
+		.src('app/pug/pages/*.pug')
+	.pipe(pug({ pretty: true }).on("error", notify.onError()))
+	.pipe(gulp.dest('app'))
+	.pipe(browserSync.reload({ stream: true }))
+});
+
+gulp.task('styles', function() {
+	return gulp.src('app/sass/**/*.sass')
+	.pipe(sass({ outputStyle: 'expand' }).on("error", notify.onError()))
+	.pipe(rename({ suffix: '.min', prefix : '' }))
+	.pipe(autoprefixer(['last 15 versions']))
+	.pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Min css. Your can commented rhis...
+	.pipe(gulp.dest('app/css'))
+	.pipe(browsersync.reload( {stream: true} ))
+});
+
+gulp.task('js', function() {
+	return gulp.src([
+		'app/js/common.js',
+		])
+	.pipe(babel({ presets: ['@babel/env'] }))
+	.pipe(concat('scripts.min.js'))
+	.pipe(uglify()) // Min JS
+	.pipe(gulp.dest('app/js'))
+	.pipe(browsersync.reload({ stream: true }))
+});
+
+gulp.task('code', function() {
+	return gulp.src('app/*.html')
+	.pipe(browsersync.reload({ stream: true }))
+});
+
+// SSH deploy...
+gulp.task('rsync', function() {
+	return gulp.src('app/**')
+	.pipe(rsync({
+		root: 'app/',
+		hostname: 'username@yousite.com',
+		destination: 'yousite/public_html/',
+		// include: ['*.htaccess'],
+		exclude: ['**/Thumbs.db', '**/*.DS_Store'],
+		recursive: true,
+		archive: true,
+		silent: false,
+		compress: true
+	}))
+});
+
+gulp.task('watch', function() {
+	gulp.watch('app/pug/**/*.pug', gulp.parallel('pug'));
+	gulp.watch('app/sass/**/*.sass', gulp.parallel('styles'));
+	gulp.watch(['libs/**/*.js', 'app/js/common.js'], gulp.parallel('js'));
+	gulp.watch('app/*.html', gulp.parallel('code'))
+});
+
+gulp.task('default', gulp.parallel('watch', 'browser-sync'));
+
+
+gulp.task('build', function() {
+	var removeDist = del.sync('dist');
+	var buildFiles = gulp.src([
+		'app/*.html',
+		'app/.htaccess',
+		]).pipe(gulp.dest('dist'));
+
+	var buildCss = gulp.src([
+		'app/css/main.min.css',
+		]).pipe(gulp.dest('dist/css'));
+
+	var buildJs = gulp.src([
+		'app/js/scripts.min.js',
+		]).pipe(gulp.dest('dist/js'));
+
+	var buildFonts = gulp.src([
+		'app/fonts/**/*',
+		]).pipe(gulp.dest('dist/fonts'));
+
+	var buildImages = gulp.src([
+		'app/img/**/*',
+		]).pipe(gulp.dest('dist/img'));
+});
